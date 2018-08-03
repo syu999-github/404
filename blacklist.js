d36dd1dcb5632f53b98d2b1a2f737ecb70121bfe
cr4fun <38368200+cr4fun@users.noreply.github.com>
2018-08-03 23:20:38 +0800

    Update blacklist.js

--- blacklist.js
+++ blacklist.js
@@ -3,5 +3,26 @@ blacklist=[
   'boards.4chan.org/gif',
   '69-porno.ru',
   'p.im9.eu',
-  'ryabina72.ru'
+  'ryabina72.ru',
+  'freeporn.com',
+  'legalporno.com',
+  'theporndude.com'
+  'pornhd.com',
+  'motherless.com',
+  'collectionofbestporn.com',
+  'spankbang.com',
+  'xxxstreams.org',
+  '123xxx.xyz',
+  '3movs.com',
+  'cn.ax101.ws',
+  'yourporn.sexy',
+  'porntrex.com',
+  'porns.land',
+  'veporns.com',
+  'veporns.com',
+  'vidz7.com',
+  'pornqd.com',
+  'porndoe.com',
+  'bobs-tube.com',
+  'theporndude.com'
 ]
